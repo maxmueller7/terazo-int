589be428bbd49321d78411774da50a4ae0db7cac
Max Mueller <xpmueller@gmail.com>
2021-09-08 15:07:26 -0400

    handling events for post and patch

--- src/components/AddInventoryDialog.tsx
+++ src/components/AddInventoryDialog.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import {
   Dialog,
   DialogTitle,
@@ -10,6 +10,7 @@ import {
   makeStyles,
   Theme,
 } from '@material-ui/core';
+import axios, { AxiosResponse, AxiosError } from 'axios';
 
 const useStyles = makeStyles((theme?: Theme) => ({
   textFieldMargingLeft: {
@@ -17,18 +18,75 @@ const useStyles = makeStyles((theme?: Theme) => ({
   },
 }));
 
-export const AddInventoryDialog: FC<{ open: boolean; setOpen: any }> = (
-  props
-): JSX.Element => {
+export const AddInventoryDialog: FC<{
+  open: boolean;
+  setOpen: any;
+  loading: boolean;
+  setLoading: any;
+  selectedWarehouseId: string;
+}> = (props): JSX.Element => {
+  const [formWarehouseId, setFormWarehouseId] = useState<string>(
+    props.selectedWarehouseId
+  );
+  const [itemSKU, setItemSKU] = useState<string>('');
+  const [itemQuantity, setItemQuantity] = useState<string>('');
+  const [itemName, setItemName] = useState<string>('');
+  const [itemDescription, setItemDescription] = useState<string>('');
   const classes = useStyles();
 
   const handleClose = () => {
     props.setOpen(false);
   };
 
-  const handleSubmitAddInventory = (e: any) => {
+  const handleWarehouseId = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFormWarehouseId(event.target.value);
+  };
+
+  const handleItemSKU = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setItemSKU(event.target.value);
+  };
+
+  const handleItemQuantity = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setItemQuantity(event.target.value);
+  };
+
+  const handleItemName = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setItemName(event.target.value);
+  };
+
+  const handleItemDescription = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setItemDescription(event.target.value);
+  };
+
+  const handleSubmitAddInventory = async (e: any) => {
+    props.setLoading(true);
     e.preventDefault();
-    console.log(e);
+    const postRequest = {
+      warehouseId: formWarehouseId,
+      itemSKU,
+      itemQuantity,
+      itemName,
+      itemDescription,
+    };
+
+    await axios
+      .post(`http://localhost:3001/${formWarehouseId}/inventory`, postRequest)
+      .then((response: AxiosResponse) => {
+        if (response.status === 200) {
+          alert('Inventory added successfully!');
+          setFormWarehouseId(props.selectedWarehouseId);
+          setItemDescription('');
+          setItemName('');
+          setItemQuantity('');
+          setItemSKU('');
+        }
+      })
+      .catch((error: AxiosError) => {
+        alert(`Error while adding inventory!\n${error.message}`);
+      })
+      .finally(props.setLoading(false));
   };
 
   return (
@@ -43,21 +101,45 @@ export const AddInventoryDialog: FC<{ open: boolean; setOpen: any }> = (
           <DialogContentText>Add inventory to a warehouse.</DialogContentText>
           <TextField
             autoFocus
+            defaultValue={props.selectedWarehouseId}
             id='warehouseId'
             label='Warehouse ID'
+            onChange={handleWarehouseId}
             type='number'
           />
           <TextField
             className={classes.textFieldMargingLeft}
-            id='itemId'
-            label='Item ID'
-            type='number'
-          />
-          <TextField
-            className={classes.textFieldMargingLeft}
+            defaultValue={itemSKU}
             id='itemSKU'
             label='Item SKU'
+            onChange={handleItemSKU}
             type='number'
+          />
+          <TextField
+            className={classes.textFieldMargingLeft}
+            defaultValue={itemQuantity}
+            id='itemQuantity'
+            label='Item Quantity'
+            onChange={handleItemQuantity}
+            type='number'
+          />
+          <br />
+          <TextField
+            defaultValue={itemName}
+            fullWidth
+            id='itemName'
+            label='Item Name'
+            onChange={handleItemName}
+            type='string'
+          />
+          <br />
+          <TextField
+            defaultValue={itemDescription}
+            fullWidth
+            id='itemDescription'
+            label='Item Description'
+            onChange={handleItemDescription}
+            type='string'
           />
           <br />
         </DialogContent>
--- src/components/EditInventoryDialog.tsx
+++ src/components/EditInventoryDialog.tsx
@@ -12,6 +12,7 @@ import {
   makeStyles,
   Theme,
 } from '@material-ui/core';
+import axios, { AxiosError, AxiosResponse } from 'axios';
 
 const useStyles = makeStyles((theme?: Theme) => ({
   textFieldMargingLeft: {
@@ -19,10 +20,22 @@ const useStyles = makeStyles((theme?: Theme) => ({
   },
 }));
 
-export const EditInventoryDialog: FC<{ open: boolean; setOpen: any }> = (
-  props
-): JSX.Element => {
-  const [deleteCheckBox, setDeleteCheckBox] = useState<boolean>(false);
+export const EditInventoryDialog: FC<{
+  open: boolean;
+  setOpen: any;
+  loading: boolean;
+  setLoading: any;
+  selectedWarehouseId: string;
+}> = (props): JSX.Element => {
+  const [formWarehouseId, setFormWarehouseId] = useState<string>(
+    props.selectedWarehouseId
+  );
+  const [itemId, setItemId] = useState<string>('');
+  const [itemSKU, setItemSKU] = useState<string>('');
+  const [itemQuantity, setItemQuantity] = useState<string>('');
+  const [itemName, setItemName] = useState<string>('');
+  const [itemDescription, setItemDescription] = useState<string>('');
+  const [itemDelete, setItemDelete] = useState<boolean>(false);
 
   const classes = useStyles();
 
@@ -30,13 +43,67 @@ export const EditInventoryDialog: FC<{ open: boolean; setOpen: any }> = (
     props.setOpen(false);
   };
 
-  const handleCheck = () => {
-    setDeleteCheckBox(!deleteCheckBox);
+  const handleWarehouseId = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFormWarehouseId(event.target.value);
   };
 
-  const handleSubmitEditInventory = (e: any) => {
+  const handleItemId = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setItemId(event.target.value);
+  };
+
+  const handleItemSKU = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setItemSKU(event.target.value);
+  };
+
+  const handleItemQuantity = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setItemQuantity(event.target.value);
+  };
+
+  const handleItemName = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setItemName(event.target.value);
+  };
+
+  const handleItemDescription = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setItemDescription(event.target.value);
+  };
+
+  const handleCheckBox = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setItemDelete(!itemDelete);
+  };
+
+  const handleSubmitEditInventory = async (e: any) => {
+    props.setLoading(true);
     e.preventDefault();
-    console.log(e);
+    const patchRequest = {
+      warehouseId: formWarehouseId,
+      itemId,
+      itemSKU,
+      itemQuantity,
+      itemName,
+      itemDescription,
+      itemDelete,
+    };
+
+    await axios
+      .patch(`http://localhost:3001/${formWarehouseId}/inventory`, patchRequest)
+      .then((response: AxiosResponse) => {
+        if (response.status === 200) {
+          alert('Saved successfully!');
+          setFormWarehouseId(props.selectedWarehouseId);
+          setItemDelete(false);
+          setItemDescription('');
+          setItemId('');
+          setItemName('');
+          setItemQuantity('');
+          setItemSKU('');
+        }
+      })
+      .catch((error: AxiosError) => {
+        alert(`Error while editing inventory!\n${error.message}`);
+      })
+      .finally(props.setLoading(false));
   };
 
   return (
@@ -50,37 +117,60 @@ export const EditInventoryDialog: FC<{ open: boolean; setOpen: any }> = (
         <DialogContent>
           <DialogContentText>Edit inventory details here.</DialogContentText>
           <TextField
+            onChange={handleWarehouseId}
+            defaultValue={props.selectedWarehouseId}
             autoFocus
             id='warehouseId'
             label='Warehouse ID'
             type='number'
           />
           <TextField
+            onChange={handleItemId}
+            defaultValue={itemId}
             className={classes.textFieldMargingLeft}
             id='itemId'
             label='Item ID'
             type='number'
           />
           <TextField
+            onChange={handleItemSKU}
+            defaultValue={itemSKU}
             className={classes.textFieldMargingLeft}
             id='itemSKU'
             label='Item SKU'
             type='number'
           />
           <br />
-          <TextField id='itemQuantity' label='Item Quantity' type='number' />
           <TextField
+            onChange={handleItemQuantity}
+            defaultValue={itemQuantity}
+            id='itemQuantity'
+            label='Item Quantity'
+            type='number'
+          />
+          <TextField
+            onChange={handleItemName}
+            defaultValue={itemName}
             className={classes.textFieldMargingLeft}
             id='itemName'
             label='Item Name'
             type='string'
           />
           <br />
+          <TextField
+            onChange={handleItemDescription}
+            defaultValue={itemDescription}
+            id='itemDescription'
+            label='Item Description'
+            type='string'
+            fullWidth
+          />
+          <br />
           <Typography style={{ marginTop: 10 }}>
             Delete?
             <Checkbox
-              checked={deleteCheckBox}
-              onChange={handleCheck}
+              checked={itemDelete}
+              onChange={handleCheckBox}
               name='delete'
             />
           </Typography>
